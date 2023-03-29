cf900ad07c000ff12473cd022eeed35c4d200a63
Chalini11 <chalini.pasupathy@m2pfintech.com>
2023-03-29 08:47:52 +0530

    Intial commit. Created required class for futher implementation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Movie Time");
+        Movie movie = new Movie("Casino Royale","Eon Productions", "PG-13");
+        movie.printDetails();
     }
 }
 
--- src/Movie.java
+++ src/Movie.java
@@ -10,4 +10,36 @@ public class Movie
         this.studio = studio;
         this.title = title;
     }
+
+    Movie(String title, String studio)
+    {
+        this.studio = studio;
+        this.title = title;
+        this.rating = "PG";
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public Movie[] getPg(Movie[] movies)
+    {
+        Movie[] pgMovies = new Movie[movies.length];
+        int index =0;
+        for(int iterator = 0; iterator < movies.length; iterator++)
+        {
+            if(movies[iterator].getRating() == "PG")
+            {
+                pgMovies[index]=movies[iterator];
+                index++;
+            }
+        }
+        return pgMovies;
+    }
+
+    public void printDetails()
+    {
+        System.out.println(title);
+        System.out.println(studio+"   "+rating);
+    }
 }
