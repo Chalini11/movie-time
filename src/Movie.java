e6cff0133fd6e35259e0224a7b08da987ba3d7a5
Chalini11 <chalini.pasupathy@m2pfintech.com>
2023-03-29 08:28:01 +0530

    Intial commit. Created required class for futher implementation

---
+++ src/Movie.java
@@ -0,0 +1,13 @@
+public class Movie
+{
+    private String title;
+    private String studio;
+    private String rating;
+
+    Movie(String title, String studio, String rating)
+    {
+        this.rating = rating;
+        this.studio = studio;
+        this.title = title;
+    }
+}
